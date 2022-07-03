50d2ac3525bba0cb363366eefffc5ff07cd4b8f6
Prince Kumar <princekumar931083@gmail.com>
2022-07-03 14:55:43 +0530

    solved

--- Solution1281.java
+++ Solution1281.java
@@ -1,6 +1,27 @@
+//Subtract the Product and Sum of Digits of an Integer
 class Solution1281
 {
+
+    public static int subtractProductAndSum(int n)
+    {
+        int sum=0;
+        int prod=1;
+        int rem=0;
+        
+        while(n>0)
+        {
+            rem=n%10;
+            n=n/10;
+            sum+=rem;
+            prod*=rem;
+            
+        }
+
+        return prod-sum;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println(subtractProductAndSum(234));
+        
     }
 }
